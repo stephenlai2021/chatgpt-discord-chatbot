a953638c64f6323a364b4bf39016e19b30829005
Stephen_Lai <stephenlai2021@gmail.com>
2023-01-04 17:28:05 +0800

    commit

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,51 +1,51 @@
-// Create a Discord Bot using OpenAI that interacts on the Discord Server
-import { DISCORD_TOKEN, OPENAI_KEY } from "$env/static/private";
-import { Client, GatewayIntentBits } from "discord.js";
-import { Configuration, OpenAIApi } from "openai";
+// // Create a Discord Bot using OpenAI that interacts on the Discord Server
+// import { DISCORD_TOKEN, OPENAI_KEY } from "$env/static/private";
+// import { Client, GatewayIntentBits } from "discord.js";
+// import { Configuration, OpenAIApi } from "openai";
 
-export const load = async () => {
-  // Prepare to connect to the Discord API
-  const client = new Client({
-    intents: [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.MessageContent,
-    ],
-  });
+// export const load = async () => {
+//   // Prepare to connect to the Discord API
+//   const client = new Client({
+//     intents: [
+//       GatewayIntentBits.Guilds,
+//       GatewayIntentBits.GuildMessages,
+//       GatewayIntentBits.MessageContent,
+//     ],
+//   });
 
-  // Log the bot into Discord
-  client.login(DISCORD_TOKEN);
-  console.log("ChatGPT Bot is online on Discord");
+//   // Log the bot into Discord
+//   client.login(DISCORD_TOKEN);
+//   console.log("ChatGPT Bot is online on Discord");
 
-  // Preoare connection to OpenAI API
-  const configuration = new Configuration({
-    organization: "org-CNyAxWDWmtUylw5fFDP3pLmc",
-    apiKey: OPENAI_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
+//   // Preoare connection to OpenAI API
+//   const configuration = new Configuration({
+//     organization: "org-CNyAxWDWmtUylw5fFDP3pLmc",
+//     apiKey: OPENAI_KEY,
+//   });
+//   const openai = new OpenAIApi(configuration);
 
-  // Check for when a message on discord is sent
-  client.on("messageCreate", async (message) => {
-    try {
-      // Don't respond to yourself or other bots
-      if (message.author.bot) return;
+//   // Check for when a message on discord is sent
+//   client.on("messageCreate", async (message) => {
+//     try {
+//       // Don't respond to yourself or other bots
+//       if (message.author.bot) return;
 
-      const gptRes = await openai.createCompletion({
-        model: "davinci",
-        prompt: `ChatGPT is a friendly chatbot.\n\
-  ChatGPT: Hello, how are you?\n\
-  ${message.author.username}: ${message.content}\n\
-  ChatGPT:`,
-        temperature: 0.5,
-        max_tokens: 300,
-        stop: ["ChatGPT:", "Stephen Lai:"],
-      });
+//       const gptRes = await openai.createCompletion({
+//         model: "davinci",
+//         prompt: `ChatGPT is a friendly chatbot.\n\
+//   ChatGPT: Hello, how are you?\n\
+//   ${message.author.username}: ${message.content}\n\
+//   ChatGPT:`,
+//         temperature: 0.5,
+//         max_tokens: 300,
+//         stop: ["ChatGPT:", "Stephen Lai:"],
+//       });
 
-      console.log("reply: ", gptRes.data.choices[0].text);
-      message.reply(`${gptRes.data.choices[0].text}`);
-      return { msg: gptRes.data.choices[0].text };
-    } catch (error) {
-      console.log(error);
-    }
-  });
-};
+//       console.log("reply: ", gptRes.data.choices[0].text);
+//       message.reply(`${gptRes.data.choices[0].text}`);
+//       return { msg: gptRes.data.choices[0].text };
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   });
+// };
